c0ea31b2b71b0d1825c60f6e34e3594f8ff281c5
sumitshinde-84 <shindesumit217@gmail.com>
2022-11-26 22:53:34 +0530

    versus section added

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-const progressBar = document.getElementsByClassName('progress-bar')[0];
-    setInterval(()=>{
+    // set up text to print, each item in array is new line
+    var aText = new Array(
+        "Select the option with you want to play:", 
+        
+        );
+        var iSpeed = 100; // time delay of print out
+        var iIndex = 0; // start printing array at this posision
+        var iArrLength = aText[0].length; // the length of the text array
+        var iScrollAt = 10; // start scrolling up at this many lines
+         
+        var iTextPos = 0; // initialise text position
+        var sContents = ''; // initialise contents variable
+        var iRow; // initialise current row
+         
+        function typewriter()
+        {
+         sContents =  ' ';
+         iRow = Math.max(0, iIndex-iScrollAt);
+         var destination = document.getElementById("typedtext");
+         
+         while ( iRow < iIndex ) {
+          sContents += aText[iRow++] + '<br />';
+         }
+         destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+         if ( iTextPos++ == iArrLength ) {
+          iTextPos = 0;
+          iIndex++;
+          if ( iIndex != aText.length ) {
+           iArrLength = aText[iIndex].length;
+           setTimeout("typewriter()", 500);
+          }
+         } else {
+          setTimeout("typewriter()", iSpeed);
+         }
+        }
 
-        const computedStyle = getComputedStyle(progressBar);
-        const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0;
-        progressBar.style.setProperty('--width', width + .1)
-    }, 5)
+        typewriter();
