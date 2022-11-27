606651c5ea19006a7cbce7aee159d287c14ca899
sumitshinde-84 <shindesumit217@gmail.com>
2022-11-27 10:55:11 +0530

    typewriter animation added by css

--- script.js
+++ script.js
@@ -1,37 +1,31 @@
-    // set up text to print, each item in array is new line
-    var aText = new Array(
-        "Select the option with you want to play:", 
+let ticTacToe = (function(){
+
+
+   
+
+    const progressBar = document.getElementsByClassName('progress-bar')[0];
+    setInterval(()=>{
+
+        const computedStyle = getComputedStyle(progressBar);
+        const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0;
+        progressBar.style.setProperty('--width', width + .1)
         
-        );
-        var iSpeed = 100; // time delay of print out
-        var iIndex = 0; // start printing array at this posision
-        var iArrLength = aText[0].length; // the length of the text array
-        var iScrollAt = 10; // start scrolling up at this many lines
-         
-        var iTextPos = 0; // initialise text position
-        var sContents = ''; // initialise contents variable
-        var iRow; // initialise current row
-         
-        function typewriter()
-        {
-         sContents =  ' ';
-         iRow = Math.max(0, iIndex-iScrollAt);
-         var destination = document.getElementById("typedtext");
-         
-         while ( iRow < iIndex ) {
-          sContents += aText[iRow++] + '<br />';
-         }
-         destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
-         if ( iTextPos++ == iArrLength ) {
-          iTextPos = 0;
-          iIndex++;
-          if ( iIndex != aText.length ) {
-           iArrLength = aText[iIndex].length;
-           setTimeout("typewriter()", 500);
-          }
-         } else {
-          setTimeout("typewriter()", iSpeed);
-         }
-        }
+    }, 5)
+  
+    const text = document.querySelector('.text');
+    const versus = document.querySelector('.versus')
+
+    window.addEventListener('load',()=>{
+        setTimeout(() => {
+            text.style.display='none'
+            progressBar.style.display='none'
+            versus.style='display:block'
+           
+            
+        }, 6000);
 
        
+      
+    })
+
+})()
