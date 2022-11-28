0d6208769b7982d2527cbb8661a8fda41eedd526
sumitshinde-84 <shindesumit217@gmail.com>
2022-11-28 22:57:05 +0530

    player hover and animation added

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let ticTacToe = (function(){
 
 
    
-
+    const turnMsg = document.querySelector('#turnMsg');
     const progressBar = document.getElementsByClassName('progress-bar')[0];
     setInterval(()=>{
 
@@ -11,6 +11,18 @@ let ticTacToe = (function(){
         progressBar.style.setProperty('--width', width + .1)
         
     }, 5)
+    const playerPlate1 = document.querySelector('.plate1');
+    const playerPlate2 = document.querySelector('.plate2');
+    const Readybtn = document.querySelector('#ready')
+    Readybtn.addEventListener('click',()=>{
+        playerPlate1.style="height: 100px;width: 300px;border: 3px var(--Dark-accent) solid; border-radius: 4px;position: absolute;top:210px; transform: translate(-450px,0);opacity:1"
+        playerPlate2.style="height: 100px;width: 300px;border: 3px var(--Dark-accent) solid; border-radius: 4px;position: absolute;top:210px; transform: translate(450px,0);opacity:1"
+        setTimeout(()=>{
+            turnMsg.textContent='Now it\'s your turn player1 '
+            playerPlate1.style="height: 100px;width: 300px;border: 3px orange solid; border-radius: 4px;position: absolute;top:210px; transform: translate(-450px,0);opacity:1;box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;"
+   
+        },1000)
+    })
   
     const text = document.querySelector('.text');
     const versus = document.querySelector('.versus')
@@ -56,43 +68,73 @@ let ticTacToe = (function(){
 
 let count =0;
 let startGame = function(i){
-
+   
     const Block = document.querySelector(`.block${i}`);
+    const turnMsg = document.querySelector('#turnMsg');
+
     if(Block.childElementCount>0){
         return
     }
     const para = document.createElement('p');
     let createPlayer = function(mark){
-      
+    
         
-
+        
         function addMark(){
+            
+           
             para.textContent=mark
           Block.appendChild(para)
-          count++;  
-
          
+          count++; 
+          
+         
+         
+        }
+        
+
+        function Turn(name){
+            
+            turnMsg.textContent=`Now it's your Turn ${name}`
+
         }
 
         return{
-            addMark
+            addMark,
+            Turn
+           
         }
 
     }
     player1 = createPlayer('X');
     player2 = createPlayer('O');
 
+    
+
    if(count % 2 == 0){
+    playerPlate2.style="height: 100px;width: 300px;border: 3px orange solid; border-radius: 4px;position: absolute;top:210px; transform: translate(450px,0);opacity:1;box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;"
+    playerPlate1.style="height: 100px;width: 300px;border: 3px var(--Dark-accent) solid; border-radius: 4px;position: absolute;top:210px; transform: translate(-450px,0);opacity:1"  
+        player1.Turn('player2')
        player1.addMark()
+       
    }else{
+    
+   
+    
+    playerPlate1.style="height: 100px;width: 300px;border: 3px orange solid; border-radius: 4px;position: absolute;top:210px; transform: translate(-450px,0);opacity:1;box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;;"  
+    playerPlate2.style="height: 100px;width: 300px;border: 3px var(--Dark-accent) solid; border-radius: 4px;position: absolute;top:210px; transform: translate(450px,0);opacity:1"  
+    player2.Turn('player1')
        player2.addMark()
    }
 
 }
   
+   
 
     return{
-        startGame
+        startGame,
+       
+        
     }    
 
 })()
@@ -103,4 +145,3 @@ function hop(i){
 
 }
 
-  
